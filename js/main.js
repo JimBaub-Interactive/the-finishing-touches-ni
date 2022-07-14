d17e378ff5fea7b872a9d5675db4633c44e74431
Jamie Neill <jsneill98@gmail.com>
2022-07-14 20:48:17 +0100

    Header nd content starts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
+    var date = new Date();
+    document.getElementById('copyDate').textContent = date.getFullYear();
 
+    var dropdowns = document.querySelectorAll('.dropdown-trigger');
+    var sidenavs = document.querySelectorAll('.sidenav');
+    var parallax = document.querySelectorAll('.parallax');
+    var initDrop = M.Dropdown.init(dropdowns, {coverTrigger: false, constrainWidth:false});
+    var initSidenav = M.Sidenav.init(sidenavs, {});
+    var initParallax = M.Parallax.init(parallax, {});
 });
